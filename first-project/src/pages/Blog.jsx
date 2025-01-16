2d6ae03414051669d49037cf240bf91d080c5b6c
Md-Badhon-Sheikh <badhon0854@gmail.com>
2025-01-16 12:14:48 +0600

    added tailwind first projects

---
+++ first-project/src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h1 className='text-2xl my-5 text-center font-bold'>Hello Blog Bhaiya!!</h1>
+        </div>
+    );
+};
+
+export default Blog;
