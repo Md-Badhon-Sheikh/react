cfe99c98daa40afe35a6d4a163332be36b96773c
Md-Badhon-Sheikh <badhon0854@gmail.com>
2025-02-15 20:35:43 +0600

    added more then

--- es6-modules-overview/src/Components/Bottle/Bottle.jsx
+++ es6-modules-overview/src/Components/Bottle/Bottle.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const Bottle = ({bottle}) => {
-    const {brand, image, price} = bottle;
-    console.log(bottle)
+const Bottle = ({ bottle, handleAddToCart }) => {
+    const { brand, image, price } = bottle;
+    // console.log(bottle);
+    console.log(handleAddToCart);
+    
     return (
-        <div>
-
-            <div className='border-2 p-2 rounded-xl text-center'>
-                <h2 className='text-xl'>Brand: {brand}</h2>
-                <img className='w-48 h-48' src={image} alt="Img" />
-                <p>Price: {price}</p>
+        <div className='border-2 p-4 rounded-xl text-center'>
+            <h2 className='text-xl font-bold'>Brand: {brand}</h2>
+            <div className='flex justify-center'>
+                <img className='w-48 h-48' src={image} alt="Bottle" />
+            </div>
+            <p className='mt-2'>Price: {price}</p>
+            <div>
+                <button className='border-2 px-3 py-1 rounded-lg hover:bg-red-200 bg-green-300' onClick={handleAddToCart}>Purchase</button>
             </div>
         </div>
     );
--- es6-modules-overview/src/Components/Bottles/Bottles.jsx
+++ es6-modules-overview/src/Components/Bottles/Bottles.jsx
@@ -3,6 +3,7 @@ import Bottle from "../Bottle/Bottle";
 
 const Bottles = () => {
   const [waterBottles, setWaterBottles] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("bottles.json")
@@ -10,12 +11,22 @@ const Bottles = () => {
       .then((data) => setWaterBottles(data));
   }, []);
 
+  const handleAddToCart = bottle => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+
+  }
+
   return (
     <>
        <h1>Bottles: {waterBottles.length}</h1>
+       <h2>cart: {cart.length} </h2>
     <div className="grid grid-cols-4 gap-3">
       {waterBottles.map(bottle => (
-        <Bottle bottle={bottle}></Bottle>
+        <Bottle
+         bottle={bottle}
+         handleAddToCart = {handleAddToCart}
+         ></Bottle>
       ))}
     </div>
     </>
